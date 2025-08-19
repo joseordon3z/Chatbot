8ae615fe7edcc8a934167de0af139e4bad5f4cd4
joseordon3z <joseramonordonez010@gmail.com>
2025-08-18 18:39:43 -0600

    Initial commit

---
+++ icons.js
@@ -0,0 +1,32 @@
+export const darkModeText = "dark_mode";
+export const lightModeText = "light_mode";
+
+export const contentCopyText = "content_copy";
+export const doneText = "done";
+
+export const volumeUpText = "volume_up";
+export const volumeOffText = "volume_off";
+
+export const sendIcon = `
+    <span class="material-symbols-rounded">send</span>
+`;
+export const lightModeIcon = `
+    <span id="mode" class="material-symbols-rounded">
+        light_mode
+    </span>
+`;
+export const deleteIcon = `
+    <span id="delete" class="material-symbols-rounded">
+        delete
+    </span>
+`;
+export const copyIcon = `
+    <span class="material-symbols-rounded copy">
+        content_copy
+    </span>
+`;
+export const volumeUpIcon = `
+    <span class="material-symbols-rounded volume-up">
+        volume_up
+    </span>
+`;
